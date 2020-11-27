ba060342b166f0084b14d8450e3978a45fba8c2b
jorenham <jhammudoglu@gmail.com>
2020-11-27 18:18:12 +0100

    __all__ for better type hinting

--- unbuiltins/__init__.py
+++ unbuiltins/__init__.py
@@ -1,3 +1,12 @@
+__all__ = [
+    "filternone",
+    "first",
+    "frozendict",
+    "either",
+    "Missable",
+    "Missing",
+]
+
 from .constants import Missable, Missing  # noqa: F401
 from .not_collections import frozendict  # noqa: F401
 from .not_itertools import either, filternone, first  # noqa: F401
